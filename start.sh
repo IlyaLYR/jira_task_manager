d1ec28e804a95ed41f7baa6b838be685ac397659
IlyaLYR <odinaev.id2005@gmail.com>
2026-04-28 22:10:51 +0300

    start.sh - точка входа

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KEY_DIR="src/main/resources"  # или просто "." если хранить в корне проекта
+PUB_KEY="$KEY_DIR/publicKey.pem"
+PRIV_KEY="$KEY_DIR/privateKey.pem"
+
+mkdir -p "$KEY_DIR"
+
+if [ ! -f "$PRIV_KEY" ] || [ ! -f "$PUB_KEY" ]; then
+    echo "🔐 Generating RSA key pair..."
+    openssl genrsa -out "$PRIV_KEY" 2048
+    openssl rsa -in "$PRIV_KEY" -pubout -out "$PUB_KEY"
+    echo "✅ Keys saved to $KEY_DIR/"
+else
+    echo "✓ Keys already exist."
+fi
+
+docker-compose up -d
+./gradlew quarkusDev
